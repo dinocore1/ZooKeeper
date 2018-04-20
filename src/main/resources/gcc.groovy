907ce21f125c6429f073135b1871fc81de3e107e
Paul Soucy <psoucy@sciaps.com>
2018-04-20 17:35:22 -0400

    wip

--- src/main/resources/gcc.groovy
+++ src/main/resources/gcc.groovy
@@ -1,16 +1,38 @@
 
-import com.devsmart.zookeeper.plugins.DefaultGCCCompiler
+import com.devsmart.zookeeper.plugins.*
 import com.devsmart.zookeeper.Platform
-import com.devsmart.zookeeper.projectmodel.BuildableExecutable
 
-import java.io.File;
+def debugCPPSettings = new CompileSettings()
+debugCPPSettings.flags.addAll(['-g', '-O0'])
 
+def debugCSettings = new CompileSettings()
+debugCSettings.flags.addAll(['-g', '-O0'])
 
-def debugVariant = new DefaultGCCCompiler()
-debugVariant.platform = Platform.getNativePlatform()
-debugVariant.cmd = 'gcc'
-debugVariant.variant = 'debug'
-debugVariant.cflags = ['-g', '-O0']
-debugVariant.includes = project.files('src', 'include')
+def staticLibDebug = new GCCStaticLibVisitor()
+staticLibDebug.platform = Platform.getNativePlatform()
+staticLibDebug.variant = 'debug'
+staticLibDebug.cppCmd = 'g++'
+staticLibDebug.cppSettings = debugCPPSettings
+staticLibDebug.cCmd = 'gcc'
+staticLibDebug.cSettings = debugCSettings
+staticLibDebug.linkCmd = 'ar'
 
-project.zooKeeper.projectVisitors.add(debugVariant)
+project.zooKeeper.projectVisitors.add(staticLibDebug)
+
+//////// Shared Libs ///////
+debugCPPSettings = new CompileSettings()
+debugCPPSettings.flags.addAll(['-g', '-O0', '-fPIC'])
+
+debugCSettings = new CompileSettings()
+debugCSettings.flags.addAll(['-g', '-O0', '-fPIC'])
+
+def sharedLibDebug = new GCCSharedLibVisitor()
+sharedLibDebug.platform = Platform.getNativePlatform()
+sharedLibDebug.variant = 'debug'
+sharedLibDebug.cppCmd = 'g++'
+sharedLibDebug.cppSettings = debugCPPSettings
+sharedLibDebug.cCmd = 'gcc'
+sharedLibDebug.cSettings = debugCSettings
+sharedLibDebug.linkCmd = 'gcc'
+
+project.zooKeeper.projectVisitors.add(sharedLibDebug)
