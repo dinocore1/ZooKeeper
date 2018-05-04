a92e7d8693d90db2cd76bde60910485ab044c022
Paul Soucy <paul@dev-smart.com>
2018-05-04 12:09:19 -0400

    added conditional build logic based on file timestamp

--- src/main/groovy/com/devsmart/zookeeper/tasks/CompileChildProcessTask.java
+++ src/main/groovy/com/devsmart/zookeeper/tasks/CompileChildProcessTask.java
@@ -3,6 +3,8 @@ package com.devsmart.zookeeper.tasks;
 import com.devsmart.zookeeper.plugins.CompileContext;
 import com.devsmart.zookeeper.plugins.CompileProcessModifier;
 import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -10,6 +12,8 @@ import java.util.List;
 import java.util.Map;
 
 public class CompileChildProcessTask extends ChildProcessTask {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompileChildProcessTask.class);
 
     public interface Delegate {
         String[] getCommandLine(CompileChildProcessTask task);
@@ -38,6 +42,29 @@ public class CompileChildProcessTask extends ChildProcessTask {
         mModifiers.add(modifier);
     }
 
+    private long latestInputFileTime() {
+        long retval = 0;
+        for(File f : getInput()) {
+            if(f.exists()) {
+                retval = Math.max(retval, f.lastModified());
+            }
+        }
+        return retval;
+    }
+
+    @Override
+    public boolean run() {
+        File outputFile = getOutput().getSingleFile();
+
+        if(!outputFile.exists()
+                || latestInputFileTime() > outputFile.lastModified() ) {
+            return super.run();
+        } else {
+            LOGGER.info("{} is up to date", outputFile.getName());
+            return true;
+        }
+
+    }
 
     @Override
     public File getWorkingDir() {
